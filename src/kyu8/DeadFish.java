8e53708defd507a22bc7882ecfa301b27a262580
Kacper Piasecki <k.r.piasecki2@gmail.com>
2023-10-11 08:47:22 +0200

    ex: [DeadFish] completed successfully

---
+++ src/kyu8/DeadFish.java
@@ -0,0 +1,58 @@
+package kyu8;
+//Write a simple parser that will parse and run Deadfish.
+//
+//        Deadfish has 4 commands, each 1 character long:
+//
+//        i increments the value (initially 0)
+//        d decrements the value
+//        s squares the value
+//        o outputs the value into the return array
+//        Invalid characters should be ignored.
+//
+//        Deadfish.parse("iiisdoso") =- new int[] {8, 64};
+
+
+import java.util.Arrays;
+
+public class DeadFish {
+
+    public static void main(String[] args) {
+        parse("iiisdosodddddiso");
+    }
+
+    public static int[] parse(String data) {
+        String[] split = data.split("");
+        System.out.println("split : " + Arrays.toString(split));
+        long length = Arrays.stream(split)
+                .filter("o"::equals)
+                .count();
+        int[] result = new int[(int)length];
+        System.out.println("result : " + Arrays.toString(result));
+        int tempSum = 0;
+        int i = 0;
+        for (String s : split) {
+            switch (s){
+                case "i":
+                    tempSum+=1;
+                    break;
+                case "d":
+                    tempSum--;
+                    break;
+                case "s":
+                    tempSum = (int)Math.pow(tempSum, 2);
+                    break;
+                case "o":
+                    result[i] = tempSum;
+                    i++;
+                    break;
+
+            }
+        }
+
+
+        System.out.println("result : " + Arrays.toString(result));
+        return result;
+
+    }
+
+}
